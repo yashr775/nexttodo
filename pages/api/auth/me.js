5c48ffcad03997ebefe7ecb6db3f42f28c4591b4
yashr775 <rajy29629@gmail.com>
2025-06-25 09:08:11 +0530

    get my profile api completed

---
+++ pages/api/auth/me.js
@@ -0,0 +1,18 @@
+import { checkAuth } from "@/utils/features";
+import { errorHandler } from "../../../middlewares/error";
+
+const handler = asyncError(async (req, res) => {
+    if (req.method !== "GET")
+        return errorHandler(res, 400, "Only GET Method is allowed");
+
+    const user = await checkAuth(req);
+
+    if (!user) return errorHandler(res, 401, "Login First");
+
+    res.status(200).json({
+        success: true,
+        user,
+    });
+});
+
+export default handler;
